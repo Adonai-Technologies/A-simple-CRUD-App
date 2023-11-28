c8d0e701ac9ec3ba031d03a4601bf83365f472c6
Akua Konadu <130577072+ADO-Tech-Web@users.noreply.github.com>
2023-11-28 17:34:01 +0100

    testing server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // console.log('May Node be with you')
 const express = require('express');
-const app = express;
+const app = express();
 
 app.listen(3000, function () {
     console.log('listening on 3000')
