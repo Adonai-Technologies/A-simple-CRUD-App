676c7779bae955dde678cc44e2e98bd3fa8fbaea
Akua Konadu <130577072+ADO-Tech-Web@users.noreply.github.com>
2023-11-28 18:02:19 +0100

    installed nodemon

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ app.listen(3000, function () {
     console.log('listening on 3000')
   })
 
+//   app.get('/', (req, res) => {
+//     res.send('Akua will go to heaven')
+//   })
+
   app.get('/', (req, res) => {
-    res.send('Akua will go to heaven')
+    res.sendFile(__dirname + '/index.html')
+    // Note: __dirname is the current directory you're in. Try logging it and see what you get!
+    // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
   })
+  console.log(__dirname)
