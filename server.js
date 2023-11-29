fba712419717996e2e20df15e730851ba2947cfd
Akua Konadu <130577072+ADO-Tech-Web@users.noreply.github.com>
2023-11-29 16:27:31 +0100

    Added bodyPasers

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
-// console.log('May Node be with you')
+// using express in server.js by requiring it.
+
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
-
+app.use(bodyParser.urlencoded({ extended: true }))
+// We need to create a server that browsers can connect to. We do this by using the Express’s listen method.
 app.listen(3000, function () {
-    console.log('listening on 3000')
-  })
+  console.log('listening on 3000')
+})
 
-//   app.get('/', (req, res) => {
-//     res.send('Akua will go to heaven')
-//   })
+// We normally abbreviate `request` to `req` and `response` to `res`.
+// Next, let’s change server.js so we serve up an index.html page back to the browser. 
+// To do this, we use the sendFile method that’s provided by the res object.
 
-  app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-    // Note: __dirname is the current directory you're in. Try logging it and see what you get!
-    // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
-  })
-  
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+  // Note: __dirname is the current directory you're in. Try logging it and see what you get!
+})
+
+app.post('/quotes', (req, res) => {
+  console.log('Heavenooooooooooooooooo!')
+})
+
+// Great, we know that Express is handling the form for us right now. 
+// The next question is, how do we get the input values with Express?
