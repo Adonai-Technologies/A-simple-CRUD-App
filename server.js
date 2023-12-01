5c7ba9c3c30cdf55d3806ca5520767505ef4b73f
Akua Konadu <130577072+ADO-Tech-Web@users.noreply.github.com>
2023-12-01 16:57:41 +0100

    new updates

--- server.js
+++ server.js
@@ -1,29 +1,29 @@
-// using express in server.js by requiring it.
-
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const app = express();
+const MongoClient = require('mongodb').MongoClient
+
 app.use(bodyParser.urlencoded({ extended: true }))
-// We need to create a server that browsers can connect to. We do this by using the Express’s listen method.
+
+
+app.get('/',  (request, respond) => {
+  // do something here
+   
+})
+
+app.post('/quotes',(request,respond) => {
+  // do something here
+  respond.sendFile(__dirname + '/index.html')
+  console.log(request.body)
+  
+})
+
+MongoClient.connect('mongodb://localhost:27017/', (err, client) => {
+  // ... do something here
+  if (err) return console.error(err)
+  console.log('Connected to Database')
+})
+
 app.listen(3000, function () {
   console.log('listening on 3000')
-})
-
-// We normally abbreviate `request` to `req` and `response` to `res`.
-// Next, let’s change server.js so we serve up an index.html page back to the browser. 
-// To do this, we use the sendFile method that’s provided by the res object.
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
-  // Note: __dirname is the current directory you're in. Try logging it and see what you get!
-})
-
-// The urlencoded method within body-parser tells body-parser 
-// to extract data from the <form> element and add them to the body property in the request object.
-app.post('/quotes', (req, res) => {
-  console.log('Heavenooooooooooooooooo!')
-  console.log(req.body)
-})
-
-// Great, we know that Express is handling the form for us right now. 
-// The next question is, how do we get the input values with Express?
+ })
