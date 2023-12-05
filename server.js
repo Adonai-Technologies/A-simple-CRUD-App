dfa15ef5603fb4f622ffd5fc6d6986728c259648
Akua Konadu <130577072+ADO-Tech-Web@users.noreply.github.com>
2023-12-05 16:17:16 +0100

    connected to mongoDB

--- server.js
+++ server.js
@@ -1,29 +1,37 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
 const MongoClient = require('mongodb').MongoClient
 
-app.use(bodyParser.urlencoded({ extended: true }))
+// Make sure you place body-parser before your CRUD handlers!
 
 
-app.get('/',  (request, respond) => {
-  // do something here
-   
-})
 
-app.post('/quotes',(request,respond) => {
-  // do something here
-  respond.sendFile(__dirname + '/index.html')
-  console.log(request.body)
-  
-})
+// All your handlers here...
+MongoClient.connect('mongodb+srv://ADO-Tech:akuakonadu@cluster0.nuwvfup.mongodb.net/?retryWrites=true&w=majority', { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to Database')
+    const db = client.db('My simple CRUD App')
+    
+    const quotesCollection = db.collection('quotes')
 
-MongoClient.connect('mongodb://localhost:27017/', (err, client) => {
-  // ... do something here
-  if (err) return console.error(err)
-  console.log('Connected to Database')
-})
-
-app.listen(3000, function () {
-  console.log('listening on 3000')
- })
+    app.use(bodyParser.urlencoded({ extended: true }))
+    
+    app.get('/', (req, res) => {
+        res.sendFile(__dirname + '/index.html')
+       
+    })
+    
+    
+    app.post('/quotes', (req, res) => {
+        console.log(req.body)
+    })
+    
+    
+    app.listen(3000, function () {
+        console.log('listening on 3000')
+    })
+     
+  })
+  .catch(error => console.error(error))
